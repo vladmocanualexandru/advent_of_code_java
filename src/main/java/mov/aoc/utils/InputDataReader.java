eb6f7ccf73b6812344cdda4394414a50e3acf00e
Vlad@Aqirys <vlad.mocanu.alexandru@gmail.com>
2023-01-04 14:02:25 +0200

    Started solution for 2022/16/1

--- src/main/java/mov/aoc/utils/InputDataReader.java
+++ src/main/java/mov/aoc/utils/InputDataReader.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
--- src/main/java/mov/aoc/y2022/d16/Part1Solution.java
+++ src/main/java/mov/aoc/y2022/d16/Part1Solution.java
@@ -2,7 +2,6 @@ package mov.aoc.y2022.d16;
 
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
